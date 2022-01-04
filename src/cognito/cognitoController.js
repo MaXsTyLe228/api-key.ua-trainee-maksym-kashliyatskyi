46559fa0ec396e63f8d5addaf3b95f453b66c2e2
MaXsTyLe228 <kashlyatsky@gmail.com>
2022-01-04 15:35:49 +0200

    signUp

--- src/cognito/cognitoController.js
+++ src/cognito/cognitoController.js
@@ -1,10 +1,15 @@
-//const Login = require('./signUp')
+const Login = require('./signUp')
 const Auth = require('./signIn')
 const LogOut = require('./logout')
 
 exports.signIn = async (event, context, callback) => {
     const data = JSON.parse(event.body);
     return await Auth.signIn(data.email, data.password)
+};
+
+exports.signUp = async (event, context, callback) => {
+    const data = JSON.parse(event.body);
+    return await Login.signUp(data.email, data.password)
 };
 
 exports.logout = async (event, context, callback) => {

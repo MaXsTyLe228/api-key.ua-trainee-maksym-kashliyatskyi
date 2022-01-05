823ec8722b525f3da4394c8ca91b715ad984e5c3
MaXsTyLe228 <kashlyatsky@gmail.com>
2022-01-05 12:20:11 +0200

    refresh refactor

--- src/cognito/refresh.js
+++ src/cognito/refresh.js
@@ -1,10 +1,16 @@
 const AmazonCognitoIdentity = require("amazon-cognito-identity-js");
+const jwt_decode = require("jwt-decode");
 const CognitoRefreshToken = require('amazon-cognito-identity-js').CognitoRefreshToken;
 
 const userPool = new AmazonCognitoIdentity.CognitoUserPool({
     UserPoolId: process.env.USER_POOL_ID,
     ClientId: process.env.CLIENT_ID
 });
+
+function decodeJWTToken(token) {
+    const {email, exp, auth_time, token_use, sub} = jwt_decode(token.idToken);
+    return {token, email, exp, uid: sub, auth_time, token_use};
+}
 
 module.exports.returnRes = (refreshToken) => {
     const cognitoUser = new AmazonCognitoIdentity.CognitoUser({
@@ -17,7 +23,16 @@ module.exports.returnRes = (refreshToken) => {
             if (err) {
                 return resolve({statusCode: 422, body: JSON.stringify(err)});
             }
-            return resolve({statusCode: 200, body: JSON.stringify(result)})
+            const token = {
+                accessToken: result.getAccessToken().getJwtToken(),
+                idToken: result.getIdToken().getJwtToken(),
+                refreshToken: result.getRefreshToken().getToken(),
+            }
+            resolve({
+                statusCode: 200,
+                headers: {"Access-Control-Allow-Origin": "*"},
+                body: JSON.stringify(decodeJWTToken(token))
+            });
         })
     })
 }
